f60d3053d958f46c60dc92907b16cef0a8b90818
pakerchang <pakerchang@gmail.com>
2021-04-07 23:50:24 +0800

    rename website title

--- src/Header.js
+++ src/Header.js
@@ -19,6 +19,7 @@ function Header() {
 				src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
 				alt="Netflix Logo"
 				className="header__logo"
+				href="#"
 			/>
 
 			<img
--- src/requests.js
+++ src/requests.js
@@ -2,13 +2,13 @@ const API_KEY = "f48c83f1a2f413186327be34e394dd5c";
 
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=zh-TW`,
-	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=zh-TW&with_networks=213&with_crew&with_people&include_video=true`,
 	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=zh-TW`,
-	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-	fetchComdeyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=zh-TW&with_genres=28`,
+	fetchComdeyMovies: `/discover/movie?api_key=${API_KEY}&language=zh-TW&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&language=zh-TW&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&language=zh-TW&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&language=zh-TW&with_genres=99`,
 };
 
 export default requests;
